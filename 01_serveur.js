ba73f96c9b3b83fc31fb50113886e918bac556bc
Champagne1 <e1201583@cmaisonneuve.qc.ca>
2018-02-08 10:34:18 -0500

    Ajout de commentaires

--- 01_serveur.js
+++ 01_serveur.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = express();
 app.use(express.static('public'));
 
+// Transforme les éléments dans le fichier JSON en tableau html
 const transforme_en_tableau = (collection) => {
 	let chaine = '<table>';
 	chaine += '<thead><tr><th>Prénom</th><th>Nom</th><th>Téléphone</th><th>Courriel</th></thead>'
@@ -13,16 +14,19 @@ const transforme_en_tableau = (collection) => {
 	return chaine
 }
 
+// Route : formulaire pour afficher le formulaire
 app.get('/formulaire', function (req, res) {
  console.log(__dirname); //nom du domaine
- res.sendFile( __dirname + "/public/html/" + "01_form.htm" );
+ res.sendFile( __dirname + "/public/html/" + "01_form.htm" ); // envoie le formulaire
 })
 
+// Page d'accueil
 app.get('/', (req, res) => {
  console.log('accueil')
  res.end('<h1>Accueil</h1>')
 })
 
+// Traite le formulaire
 app.get('/traiter_get', function (req, res) {
 	// Preparer l'output en format JSON
 	console.log('la route /traiter_get')
@@ -37,14 +41,16 @@ app.get('/traiter_get', function (req, res) {
 	console.log(reponse);
 	res.end(JSON.stringify(reponse));
 
+	// Insère le nouveau membre dans un fichier texte
 	fs.appendFile('public/membres.txt', ',' + JSON.stringify(reponse), function (err) {
 		if (err) throw err;
 		console.log('Sauvegardé');
 	});
 });
 
-// Route : membres
+// Route : membres pour afficher les membres
 app.get('/membres', (req, res) => {
+	// Lit le fichier texte membres
 	fs.readFile('public/membres.txt', 'utf8', function(err, data) {
 		if (err) throw err;
 		let collection = JSON.parse('[' + data + ']');
@@ -52,10 +58,11 @@ app.get('/membres', (req, res) => {
 	});
 });
 
+// Le serveur
 var server = app.listen(8081, function () {
- var host = server.address().address
- var port = server.address().port
- 
- console.log("Exemple l'application écoute sur http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("Exemple l'application écoute sur http://%s:%s", host, port)
 
 })
